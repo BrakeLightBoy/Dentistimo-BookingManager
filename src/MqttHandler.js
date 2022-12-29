4dc3079122f4bffd2dba7d2b7d17534a5acbf592
johnbern <gusberjoid@student.gu.se>
2022-12-29 08:52:34 +0100

    Delete unused code

--- src/MqttHandler.js
+++ src/MqttHandler.js
@@ -1,5 +1,3 @@
-const clinicService = require('./services/clinic')
-
 class MqttHandler{
     constructor() {
         const mqtt = require('mqtt');
@@ -18,35 +16,7 @@ class MqttHandler{
         
         client.on('connect', () => {
             console.log(`Connected to MQTT broker with URI: ${brokerURI}`);
-
             client.subscribe('common/#',{qos:2})
-
-
-            clinicService.getAllClinics().then(res => {
-
-                for (const clinic of res){
-                    
-
-                 // console.log(`DUMMY DATA: topic: clinics/${clinic.id}/slots/2022/12 payload: ${dec_str}`)
-                    // clinicService.getClinicAppointments(clinic.id).then(appointments => {
-                    //     const months = {}
-                    //     for (const appointment of appointments){
-                    //         const month = 1 + appointment.date.getUTCMonth() 
-                    //         const year = appointment.date.getUTCFullYear()
-                    //         // console.log('Entry: '+clinic.id+"/"+month+'-'+year)
-                    //     }     
-                    //     // console.log("ap2:",appointments)
-                    // }).catch(e => {
-                    //     console.log(e)
-                    // })
-                }
-
-                console.log('publish',JSON.stringify(res))
-                client.publish('clinics',JSON.stringify(res),{qos:2, retain:true})
-            }).catch(e => {
-                console.log('Failed to get clinics')
-                console.log(e)              
-            })
         })
 
         client.on('error',error => {

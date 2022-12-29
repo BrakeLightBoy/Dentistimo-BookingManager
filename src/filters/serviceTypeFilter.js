e807cd06c646856b37d356eb06f9f66d8972b658
johnbern <gusberjoid@student.gu.se>
2022-12-29 08:54:02 +0100

    Update filters

--- src/filters/serviceTypeFilter.js
+++ src/filters/serviceTypeFilter.js
@@ -1,16 +1,8 @@
-const filterUserServ = require('./userServFilter')
-const filterDentistServ = require('./dentistServFilter')
 const filterAppointmentServ = require('./appointmentServFilter')
 
 const transform = (payload) => {
     if(payload.opCat){
         switch(payload.opCat){
-            case 'user':
-                filterUserServ(payload)
-                break;
-            case 'dentist':
-                filterDentistServ(payload)
-                break;
             case 'appointment':
                 filterAppointmentServ(payload)
                 break;
